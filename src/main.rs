c3a23e743cbc5f63ddcd5888a302c60eab1380fc
Brian Amsler <manof2words@yahoo.com>
2022-12-01 22:37:55 -0800

    did the code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{fs::File, io::{BufReader, BufRead}};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("calories.txt").unwrap();
+    let reader = BufReader::new(f);
+
+    let mut current = 0;
+    let mut max = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        match line.as_str() {
+            "" => {
+                if current > max {
+                    max = current;
+                }
+                
+                current = 0;
+            },
+            _ => {
+                let amount: i32 = line.parse().unwrap();
+                current += amount;
+            }
+        }
+    }
+
+    println!("max: {}", max);
 }
